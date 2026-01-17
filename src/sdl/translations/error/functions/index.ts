b6d72a1e3d83f05a695d08fe266aa74f289f592f
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2026-01-17 08:47:21 +0700

    chore: move error translations to its own folder

---
+++ src/sdl/translations/error/functions/index.ts
@@ -0,0 +1,22 @@
+import type { CString } from 'bun:ffi';
+import type { SDL } from '../../..';
+
+export function setError(this: SDL, messages: CString[]) {
+  return this.symbols.SDL_SetError(...messages.map((msg) => msg.ptr));
+}
+
+export function setErrorV(this: SDL) {
+  throw new Error('Not implemented');
+}
+
+export function outOfMemory(this: SDL) {
+  return this.symbols.SDL_OutOfMemory();
+}
+
+export function getError(this: SDL) {
+  return this.symbols.SDL_GetError();
+}
+
+export function clearError(this: SDL) {
+  return this.symbols.SDL_ClearError();
+}

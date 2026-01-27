34236e45d2717344141e3d2e7328c84b8cc05949
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2026-01-27 10:14:25 +0700

    fix: move all raylib shims code to assets dir

--- src/raylib/ffi/shims/loader.ts
+++ src/raylib/ffi/shims/loader.ts
@@ -1,15 +1,18 @@
+import { RAYLIB_ASSETS_PATH } from '@/utility/constant';
 import { cc } from 'bun:ffi';
 import path from 'node:path';
 import { ShimDefinition } from './definition';
+
+const SHIMS_PATH = path.join(RAYLIB_ASSETS_PATH, 'shims');
 
 export function loadShim(headerPath: string) {
   const headerDir = path.dirname(headerPath);
 
   return cc({
-    source: path.join(__dirname, 'core.c'),
+    source: path.join(SHIMS_PATH, 'core.c'),
     flags: [
       `-I${headerDir}`, // For raylib.h
-      `-I${__dirname}`, // For categories/*.c and macro.h
+      `-I${SHIMS_PATH}`, // For categories/*.c and macro.h
     ],
     symbols: ShimDefinition,
   });
--- src/utility/constant.ts
+++ src/utility/constant.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 export const IS_BIG_ENDIAN =
   new Uint8Array(new Uint16Array([0x1234]).buffer)[0] === 0x12;
 
@@ -6,3 +8,7 @@ export const IS_WINDOWS = process.platform === 'win32';
 export const WIDE_STRING_CHAR_SIZE = IS_WINDOWS ? 2 : 4;
 
 export const WIDE_STRING_MAX_SCAN = 4096;
+
+export const ASSETS_PATH = path.resolve(__dirname, '../assets');
+
+export const RAYLIB_ASSETS_PATH = path.resolve(ASSETS_PATH, 'raylib');
